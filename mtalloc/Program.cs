f26288af715760e5ece01ab901b9b4c1fcbf3946
Marc <RhinoDevel@noreply.users.github.com>
2018-09-08 00:15:07 +0200

    Allocation.

--- mtalloc/Program.cs
+++ mtalloc/Program.cs
@@ -12,13 +12,55 @@ namespace mtalloc
 
         private static ushort Alloc(ushort wantedLen)
         {
-            throw new NotImplementedException();
+            ushort nodeAddr = NodeMem.GetAllocNodeAddr(wantedLen),
+                newNodeAddr = 0;
+            Node n = null,
+                newNode = null;
+
+            if(nodeAddr == 0)
+            {
+                return 0;
+            }
+
+            n = Node.Load(nodeAddr);
+
+            if(n.BlockLen == wantedLen)
+            {
+                n.IsAllocated = 1;
+                return n.BlockAddr;
+            }
+
+            newNode = new Node();
+            newNode.BlockLen = wantedLen;
+            newNode.IsAllocated = 1;
+            newNode.LastNodeAddr = nodeAddr;
+            newNode.NextNodeAddr = n.NextNodeAddr;
+            newNode.BlockAddr = (ushort)(n.BlockAddr + n.BlockLen - wantedLen);
+
+            newNodeAddr = NodeMem.Store(newNode);
+            if(newNodeAddr == 0)
+            {
+                return 0;
+            }
+
+            n.NextNodeAddr = newNodeAddr;
+
+            if (newNode.NextNodeAddr != 0)
+            {
+                Node nextNode = Node.Load(n.NextNodeAddr);
+
+                nextNode.LastNodeAddr = newNodeAddr;
+
+                Node.Store(newNode.NextNodeAddr, nextNode);
+            }
+
+            return newNode.BlockAddr;
         }
 
         static void Main(string[] args)
         {
             NodeMem.Init();
-            //Alloc(500);
+            Alloc(400);
             //Alloc(500);
         }
     }

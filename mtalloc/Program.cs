a3596e01fef6a94363e9d96bce27e6a43ef2d6f3
Marc <RhinoDevel@users.noreply.github.com>
2018-09-18 13:48:45 +0200

    Free() (not working); Bugfix; Test code.

--- mtalloc/Program.cs
+++ mtalloc/Program.cs
@@ -8,7 +8,42 @@ namespace mtalloc
     {
         private static void Free(ushort blockAddr)
         {
-            throw new NotImplementedException();
+            ushort nodeAddr = 0;
+            Node cur = null;
+
+            if(blockAddr == 0)
+            {
+                //Debug.Assert(false);
+                return; // Nothing to do.
+            }
+
+            nodeAddr = NodeMem.GetBlockNodeAddr(blockAddr);
+            if(nodeAddr == 0)
+            {
+                Debug.Assert(false);
+                return; // No block at given (start) address is allocated.
+            }
+
+            cur = Node.Load(nodeAddr);
+
+            if(cur.IsAllocated == 0)
+            {
+                Debug.Assert(false);
+                return; // Already deallocated.
+            }
+
+            cur.IsAllocated = 0;
+            Node.Store(nodeAddr, cur);
+
+            if(cur.NextNodeAddr != 0)
+            {
+                NodeMem.MergeUnallocatedWithNextIfPossible(nodeAddr);
+            }
+            if(cur.LastNodeAddr != 0
+                && Node.Load(cur.LastNodeAddr).IsAllocated == 0)
+            {
+                NodeMem.MergeUnallocatedWithNextIfPossible(cur.LastNodeAddr);
+            }
         }
 
         private static ushort Alloc(ushort wantedLen)
@@ -42,18 +77,20 @@ namespace mtalloc
             newNode.BlockAddr = (ushort)(n.BlockAddr + n.BlockLen - wantedLen);
 
             newNodeAddr = NodeMem.Store(newNode);
+            n = null;
             if(newNodeAddr == 0)
             {
                 return 0;
             }
-
+            n = Node.Load(nodeAddr);
+            
             n.BlockLen -= newNode.BlockLen;
             n.NextNodeAddr = newNodeAddr;
             Node.Store(nodeAddr, n);
 
             if (newNode.NextNodeAddr != 0)
             {
-                Node nextNode = Node.Load(n.NextNodeAddr);
+                Node nextNode = Node.Load(newNode.NextNodeAddr);
 
                 nextNode.LastNodeAddr = newNodeAddr;
 
@@ -66,8 +103,15 @@ namespace mtalloc
         static void Main(string[] args)
         {
             NodeMem.Init();
-            Alloc(400);
-            //Alloc(500);
+            
+            var a = Alloc(10);
+            var b = Alloc(20);
+
+            
+            var c = Alloc(30);
+            Free(b);
+            Free(c);
+            Free(a);
         }
     }
 }

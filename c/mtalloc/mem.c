a0b9a0bda666ecc422dd45d4dab2d76cfa1ac19c
Marc <RhinoDevel@users.noreply.github.com>
2018-11-19 20:21:56 +0100

    mem_init() (instead of hard-coded, useless stuff).

--- c/mtalloc/mem.c
+++ c/mtalloc/mem.c
@@ -5,9 +5,11 @@
 
 #include "mem.h"
 
-uint16_t const g_mem_len = 80;
 uint16_t const g_mem_addr_first = 8; // Must not be 0?! Stupid..
-uint16_t const g_mem_heap_len = g_mem_len - g_mem_addr_first;
+uint16_t g_mem_heap_len = 0;
+
+static uint8_t * s_heap = 0;
+static uint16_t s_heap_len = 0;
 
 void mem_store_word(uint16_t const addr, uint16_t const val)
 {
@@ -27,4 +29,12 @@ void mem_store_byte(uint16_t const addr, uint8_t const val)
 uint8_t mem_load_byte(uint16_t const addr)
 {
     return 0; // MT_TODO: TEST: Implement!
+}
+
+void mem_init(uint8_t * const heap, uint16_t const heap_len)
+{
+    s_heap = heap;
+    s_heap_len = heap_len;
+
+    g_mem_heap_len = s_heap_len - g_mem_addr_first;
 }
--- c/mtalloc/mem.h
+++ c/mtalloc/mem.h
@@ -12,4 +12,8 @@ uint16_t mem_load_word(uint16_t const addr);
 void mem_store_byte(uint16_t const addr, uint8_t const val);
 uint8_t mem_load_byte(uint16_t const addr);
 
+/** Initialize singleton.
+ */
+void mem_init(uint8_t * const heap, uint16_t const heap_len);
+
 #endif //MT_MEM

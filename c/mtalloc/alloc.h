2f84e3c42e09a6c02598a7d2842b1f8ccf8f75fa
Marc <RhinoDevel@users.noreply.github.com>
2018-11-19 20:10:16 +0100

    alloc_... implementation.

---
+++ c/mtalloc/alloc.h
@@ -0,0 +1,13 @@
+
+// RhinoDevel, MT, 2018nov19
+
+#ifndef MT_ALLOC
+#define MT_ALLOC
+
+#include <stdint.h>
+
+void alloc_free(uint16_t const block_addr);
+
+uint16_t alloc_alloc(uint16_t const wanted_len);
+
+#endif //MT_ALLOC

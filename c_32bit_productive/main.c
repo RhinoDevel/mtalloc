42abdd34f7ce3d5b42f6a3940c8bbdb924ddf7ba
Marc <RhinoDevel@users.noreply.github.com>
2018-12-11 15:47:09 +0100

    Make NDEBUG build possible.

--- c_32bit_productive/main.c
+++ c_32bit_productive/main.c
@@ -10,67 +10,103 @@
 #include "mtalloc/mem.h"
 
 int main()
-{   
+{  
+    // Allocate memory via system for testing:
+    
     static size_t const mem_len = 200;
     uint8_t * const mem = malloc(mem_len * sizeof *mem);
     
     assert(sizeof *mem == 1);
+    assert(sizeof (void*) == sizeof (MT_USIGN));
 
-    printf("Node size: %d\n", (int)sizeof(struct node));
+#ifndef NDEBUG
+    printf("Node size: %d\n", (int)sizeof (struct node));
+#endif //NDEBUG
     
+#ifndef NDEBUG
     mem_clear(mem, mem_len, MT_ALLOC_DEB_CLR_1);
     mem_print(mem, mem_len);
+#endif //NDEBUG
+    
+    // Initialize memory heap manager:
     
     alloc_init(mem, mem_len);
-    
-    mem_print(mem, mem_len);
 
+#ifndef NDEBUG
+    mem_print(mem, mem_len);
+#endif //NDEBUG
+
+    // Reserve all possible memory:
+    
     void* all = alloc_alloc(mem_len - 2 * sizeof (struct node));
 
+#ifndef NDEBUG
     mem_print(mem, mem_len);
+#endif //NDEBUG
     
+#ifndef NDEBUG
     mem_clear(all, mem_len - 2 * sizeof (struct node), MT_ALLOC_DEB_CLR_2);
-
     mem_print(mem, mem_len);
     printf("***\n");
+#endif //NDEBUG
 
     alloc_free(all);
     all = 0;
 
+#ifndef NDEBUG
     mem_print(mem, mem_len);
+#endif //NDEBUG
+    
+    // Some more testing:
     
     void* a = alloc_alloc(10);
 
+#ifndef NDEBUG
     mem_print(mem, mem_len);
+#endif //NDEBUG
+    
+#ifndef NDEBUG
     mem_clear(a, 10, 170);
     mem_print(mem, mem_len);
+#endif //NDEBUG
 
     void* b = alloc_alloc(5);
 
+#ifndef NDEBUG
     mem_clear(b, 5, 187);
-
+#endif //NDEBUG
+    
     void* c = alloc_alloc(7);
 
+#ifndef NDEBUG
     mem_clear(c, 7, 204);
-
     mem_print(mem, mem_len);
     printf("-\n");
-
+#endif //NDEBUG
+    
     alloc_free(c);
     c = 0;
 
+#ifndef NDEBUG
     mem_print(mem, mem_len);
     printf("-\n");
+#endif //NDEBUG
 
     alloc_free(b);
 
+#ifndef NDEBUG
     mem_print(mem, mem_len);
-    printf("-\n");// Console.WriteLine("-");
+    printf("-\n");
+#endif //NDEBUG
 
     alloc_free(a);
 
+#ifndef NDEBUG
     mem_print(mem, mem_len);
-
+#endif //NDEBUG
+    
+    // Release allocated memory:
+    
     free(mem);
     
     return 0;
